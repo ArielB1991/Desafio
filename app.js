befa1d3b2e39b2ced102e45cf4e38465b7ad42b2
Ariel Alejandro Berurena <arielb991@gmail.com>
2025-02-03 01:30:57 -0300

    "Se las funciones en partes mas peque;as"

--- app.js
+++ app.js
@@ -1,32 +1,47 @@
-let listaDeAmigos = [];
+const listaDeAmigos = [];
 
 function agregarAmigo() {
-    let input = document.getElementById("amigo");
-    let nombre = input.value.trim(); // Elimina espacios en blanco
+    const input = document.getElementById("amigo");
+    const nombre = input.value.trim(); // Elimina espacios en blanco
 
-    if (nombre === "") {
-        alert("Por favor, inserte un nombre valido.");
+    if(!validarNombre(nombre))
         return;
+
+    listaDeAmigos.push(nombre);
+
+    actualizarLista();
+
+    borrarCampo(input)
+
+}
+
+function validarNombre(nombre){
+    if (!nombre) {
+        alert("Por favor, inserte un nombre valido.");
+        return false;
     }
 
     if (listaDeAmigos.includes(nombre)) {
         alert("Este nombre ya está en la lista.");
-        return;
+        return false;
     }
 
-    listaDeAmigos.push(nombre);
-    actualizarLista();
-    input.value = ""; // Limpiar campo de entrada
+        return true;
 }
+    
+
 
 function actualizarLista() {
     let listaHTML = document.getElementById("listaAmigos");
     listaHTML.innerHTML = ""; // Limpia la lista antes de actualizarla
 
     listaDeAmigos.forEach(amigo => {
-        let li = document.createElement("li");
+        const li = document.createElement("li");
         li.textContent = amigo;
         listaHTML.appendChild(li);
     });
 }
 
+function borrarCampo(input){
+    input.value = "";
+}
