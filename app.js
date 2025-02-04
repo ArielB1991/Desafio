fe4dfbef066521d0abe0c83db882458022196832
Ariel Alejandro Berurena <arielb991@gmail.com>
2025-02-04 01:45:37 -0300

    "Se agrega la funcion de sorteo, y comentarios en las primeras secciones del codigo"

--- app.js
+++ app.js
@@ -1,47 +1,91 @@
+// Declaramos la lista vacia
 const listaDeAmigos = [];
 
+//Declaramos la funcion para agregar nombres a la lista
 function agregarAmigo() {
-    const input = document.getElementById("amigo");
-    const nombre = input.value.trim(); // Elimina espacios en blanco
+  //Usamos elemento Dom para obtener la referencia del html
+  const input = document.getElementById("amigo");
+  // Elimina espacios en blanco
+  const nombre = input.value.trim();
 
-    if(!validarNombre(nombre))
-        return;
-
-    listaDeAmigos.push(nombre);
-
-    actualizarLista();
-
-    borrarCampo(input)
-
+  //Validador llama a la funcion validadr
+  if (!validarNombre(nombre)) return;
+  // En caso de ser valido se agrega el nombre a la lista
+  listaDeAmigos.push(nombre);
+  //Se llama a la funcion para mostrar cambios en pantalla
+  actualizarLista();
+  //Se llama a la funcio para borrar el campo de entrada despues de una inyeccion
+  borrarCampo(input);
 }
 
-function validarNombre(nombre){
-    if (!nombre) {
-        alert("Por favor, inserte un nombre valido.");
-        return false;
-    }
+function validarNombre(nombre) {
+  //Validador de campo vacio
+  if (!nombre) {
+    alert("Por favor, inserte un nombre valido.");
+    return false;
+  }
+  //Validador de nombre ya ingresado anteriormente
+  if (listaDeAmigos.includes(nombre)) {
+    alert("Este nombre ya está en la lista.");
+    return false;
+  }
 
-    if (listaDeAmigos.includes(nombre)) {
-        alert("Este nombre ya está en la lista.");
-        return false;
-    }
-
-        return true;
+  return true;
 }
-    
-
 
 function actualizarLista() {
-    let listaHTML = document.getElementById("listaAmigos");
-    listaHTML.innerHTML = ""; // Limpia la lista antes de actualizarla
-
-    listaDeAmigos.forEach(amigo => {
-        const li = document.createElement("li");
-        li.textContent = amigo;
-        listaHTML.appendChild(li);
-    });
+  //Obtiene la referencia par usar el dom elegido
+  let listaHTML = document.getElementById("listaAmigos");
+  listaHTML.innerHTML = ""; // Limpia la lista antes de actualizarla
+  //Iteramos con forEach la listaDeAmigos
+  listaDeAmigos.forEach((amigo) => {
+    //Usamos una funcionalidad del dom para crear un nuevo elemento
+    const li = document.createElement("li");
+    //Asignamos a la creacion un texto dentro
+    li.textContent = amigo;
+    //Agregamos la nueva entrada li al final de la lista
+    listaHTML.appendChild(li);
+  });
 }
 
-function borrarCampo(input){
-    input.value = "";
+function borrarCampo(input) {
+  //Vacia el campo luego de agregar el nombre
+  input.value = "";
 }
+
+function sortearAmigo(){
+    if(!validarCantidad())
+
+        return
+        
+        const resultadoHtml = document.getElementById("resultado")
+        limpiarElemento(resultadoHtml);
+
+        generarSorteo()
+    }
+
+    function generarSorteo(){
+
+        const aleatorio = Math.floor(Math.random()* listaDeAmigos.length)
+
+        const amigoSorteado = listaDeAmigos[aleatorio];
+
+        const resultadoHtml = document.getElementById("resultado");
+        const li = document.createElement("li")
+        li.textContent = `El amigo sorteado es : ${amigoSorteado}`
+        resultadoHtml.appendChild(li)
+
+    }
+
+    function validarCantidad(){
+        if(listaDeAmigos.length === 0){
+            alert("La lista esta vacia agrega mas amigos al sorteo")
+            return false;
+        }
+        return true
+    }
+
+    function limpiarElemento(elemento){
+        elemento.innerHTML = "";
+    }
+
